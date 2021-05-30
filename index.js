f9120c3613dc226534c16cdf12f15c7fa1ba85b4
gabrielvicto0802 <gabrirlvictor0802@gmail.com>
2021-05-30 12:17:24 -0300

    subindo função

---
+++ index.js
@@ -0,0 +1,41 @@
+function mensagem(ola){
+    console.log(`${ola}, para todos`)
+}
+
+mensagem("bom dia")
+
+
+
+
+function ola(nome){
+    console.log(`oi, meu nome e ${nome}`)
+}
+
+ola("gabriel")
+
+
+
+
+function gabriel(nome,idade,musica){
+    console.log(`meu nome e ${nome} e tenho ${idade} anos de idade. meu estilo musical favorito é ${musica}`)
+}
+
+gabriel("gabriel",19,"MPB")
+
+
+
+
+function lazer(filme,musica){
+    console.log(`meus filmes favoritos são os da ${filme}. Minha musica favorita e ${musica} do lagum`)
+}
+
+lazer("saga do harry potter","eu e minhas paranoias")
+
+
+
+
+function numero(calculo1,calculo2){
+    console.log(`o calculo e ` + calculo1 * calculo2)
+}
+
+numero(50,3)
